f0854a878f92fa6067a8afeee54f03267260dd7b
arora123you <31950798+arora123you@users.noreply.github.com>
2026-03-31 18:16:05 -0400

    fixed scroll hijacking

--- frontend.py
+++ frontend.py
@@ -88,7 +88,7 @@ if layer == "1. Digital Twin":
             st.plotly_chart(
                 fig_3d, 
                 use_container_width=True,  # Tells Plotly to fill the 80% column
-                config={'scrollZoom': True} 
+                config={'scrollZoom': False} 
             )
         
     except Exception as e:
